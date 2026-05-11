ddd0ffa6031951e612951d0e75d4a0a2d2a4232b
osmdmr993 <129073613+osmdmr993@users.noreply.github.com>
2026-05-11 01:10:11 +0000

    app.py

--- app.py
+++ app.py
@@ -13,4 +13,8 @@ def trending():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+    @app.route("/")
+def root():
+    return "ok", 200
+
 
